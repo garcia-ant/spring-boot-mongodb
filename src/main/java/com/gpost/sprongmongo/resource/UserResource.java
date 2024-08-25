bd50158a6018365de1eb5dad79771bb419c7232c
garcia-ant <garcantony19@gmail.com>
2024-08-25 22:07:27 +0100

    insert user

--- src/main/java/com/gpost/sprongmongo/resource/UserResource.java
+++ src/main/java/com/gpost/sprongmongo/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.gpost.sprongmongo.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gpost.sprongmongo.domain.User;
 import com.gpost.sprongmongo.dto.UserDTO;
@@ -29,10 +33,20 @@ public class UserResource {
 		return ResponseEntity.ok().body(lisDto);
 	}
 
+
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 	    User obj = service.findById(id); // Chamando o método correto que retorna um único User
 	    return ResponseEntity.ok().body(new UserDTO(obj)); // Retorna o DTO do objeto User
 	}
+	
+	@PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objdto) {
+        User obj = service.fromDTO(objdto); 
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();    
+    }
 
 }
--- src/main/java/com/gpost/sprongmongo/services/UserService.java
+++ src/main/java/com/gpost/sprongmongo/services/UserService.java
@@ -7,24 +7,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gpost.sprongmongo.domain.User;
+import com.gpost.sprongmongo.dto.UserDTO;
 import com.gpost.sprongmongo.repository.UserRepository;
 import com.gpost.sprongmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
-	private UserRepository  repository;
-	
-	public List<User> findAll(){
-		return repository.findAll();	
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
-        // Usar Optional para tratar a ausência do usuário
-        Optional<User> user = repository.findById(id);
-        
-        // Se o usuário não for encontrado, lançar exceção
-        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
-    }
+		// Usar Optional para tratar a ausência do usuário
+		Optional<User> user = repository.findById(id);
+		// Se o usuário não for encontrado, lançar exceção
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objdto) {
+		return new User(objdto.getId(), objdto.getName(), objdto.getEmail());
+	}
+
 }
